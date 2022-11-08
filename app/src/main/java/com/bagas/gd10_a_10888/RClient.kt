7942b4514a57f6d2c736acd25a7b7f577670c824
rosechill <bagasab11@gmail.com>
2022-11-08 16:11:57 +0700

    Initial commit

---
+++ app/src/main/java/com/bagas/gd10_a_10888/RClient.kt
@@ -0,0 +1,16 @@
+package com.bagas.gd10_a_10888
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+    object RClient {
+        private const val BASE_URL = "http://172.20.10.8:8080/ci4-apiserver/public/"
+        val instances:api by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            retrofit.create(api::class.java)
+        }
+    }
+
---
+++ app/src/main/java/com/bagas/gd10_a_10888/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.bagas.gd10_a_10888
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
